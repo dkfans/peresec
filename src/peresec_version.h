65a8d28e697545b432d16e7d9cbd16fb3dec3836
mefistotelis <mefistotelis@gmail.com>
2010-04-25 03:00:00 +0200

    Prepared for 1.0.9 release.
    Updated version number and readme.

--- src/peresec_version.h
+++ src/peresec_version.h
@@ -1,21 +1,43 @@
+/******************************************************************************/
+// PE/DLL Rebuilder of Export Section
+/******************************************************************************/
+/** @file peresec_version.h
+ *     Project name, version, copyrights and global definitions.
+ * @par Purpose:
+ *     Header file for global names and defines used by resource compiler.
+ * @par Comment:
+ *     Can only contain commands which resource compiler can understand.
+ * @author   Tomasz Lis
+ * @date     08 Jan 2010 - 20 Apr 2010
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#ifndef PERESEC_VERSION_H
+#define PERESEC_VERSION_H
 
-#ifndef PERESEC_PRIVATE_H
-#define PERESEC_PRIVATE_H
+/* Version definitions */
+#define VER_MAJOR         1
+#define VER_MINOR         0
+#define VER_RELEASE       9
+#define VER_BUILD         21
+#define VER_STRING        "1.0.9.21"
+/* Program name, copyrights and file names */
+#define PROGRAM_NAME      "PEReSec"
+#define PROGRAM_FULL_NAME "PE/DLL Rebuilder of Export Section"
+#define COMPANY_NAME      "Community based"
+#define INTERNAL_NAME     "peresec"
+#define LEGAL_COPYRIGHT   "GNU General Public License"
+#define LEGAL_TRADEMARKS  ""
+#define FILE_VERSION VER_STRING
+#define FILE_DESCRIPTION PROGRAM_NAME
+#define ORIGINAL_FILENAME INTERNAL_NAME".exe"
+#define PRODUCT_NAME PROGRAM_FULL_NAME
+#define PRODUCT_VERSION    VER_STRING
+#define DEFAULT_LOG_FILENAME INTERNAL_NAME".log"
 
-/* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.8.17"
-#define VER_MAJOR	1
-#define VER_MINOR	0
-#define VER_RELEASE	8
-#define VER_BUILD	17
-#define COMPANY_NAME	"Open-Source"
-#define FILE_VERSION	""
-#define FILE_DESCRIPTION	"PE/DLL Rebuilder of Export Section"
-#define INTERNAL_NAME	"PeRESec"
-#define LEGAL_COPYRIGHT	"GNU General Public License"
-#define LEGAL_TRADEMARKS	""
-#define ORIGINAL_FILENAME	""
-#define PRODUCT_NAME	"PeRESec"
-#define PRODUCT_VERSION	""
-
-#endif /*PERESEC_PRIVATE_H*/
+#endif /*PERESEC_VERSION_H*/
+/******************************************************************************/
